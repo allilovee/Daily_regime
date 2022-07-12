a707ba5434d242c4ad7103c56ed79a4aa5bc2ac1
allilovee <68789738+allilovee@users.noreply.github.com>
2022-07-12 18:02:00 +0400

    Add files via upload

---
+++ Daily_regime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Объявление пространства имен
+namespace ProgramStructure
+{
+    //Объявление класса
+    class Program
+    {
+        //Главный метод программы
+        static void Main(string[] args)
+        {
+            //Вывод строки
+            Console.WriteLine("Hello World!");
+            //Вспомогательный оператор
+            var a = Console.ReadKey();
+        }
+    }
+}
